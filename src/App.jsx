e4d34d0a6d0217ff8d57716c525b503820afafc0
Jurgita <jurgita011@gmail.com>
2023-05-11 19:00:31 +0300

    mygtukai

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss';
+import Inputs from './components/Inputs';
+//import Ls from './components/Ls';
+//import Sq4 from './components/sq4';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     { /* <Sq4/>*/} 
+      {/*<Ls />*/} 
+      <Inputs/>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
